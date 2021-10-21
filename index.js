0b04e7a33ba88ee238f8e4327d60468eade77310
Dariusz Okonski <darek200180@gmail.com>
2021-10-21 03:29:00 +0100

    add files .hbs

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const {configuratorRouter} = require("./routes/configurator");
 const {homeRouter} = require("./routes/home");
 
 const cookieParser = require('cookie-parser');
-const hbs = require('handlebars');
+const hbs = require('express-handlebars');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
-app.use(express.urlencoded({
-    extended: true,
-}));
 app.engine('.hbs', hbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 

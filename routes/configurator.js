502438fc4bfdd0da955b75b108939201583abd2e
Dariusz Okonski <darek200180@gmail.com>
2021-11-15 14:31:23 +0000

    add styles

--- routes/configurator.js
+++ routes/configurator.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const {getAddonsFromReq} = require("../utils/get-addons-from-req");
-const {COOKIE_ADDONS} = require("../data/cookies-data");
+const {COOKIE_ADDONS, COOKIE_BASES} = require("../data/cookies-data");
 
 const configuratorRouter = express.Router();
 
 configuratorRouter
     .get("/select-base/:baseName", (req, res) => {
         const { baseName } = req.params;
+
+        if(!COOKIE_BASES[baseName]) {
+            return res.render('error', {
+                description: `There is no such a base as: ${baseName}`
+            })
+        }
 
         res
             .cookie('cookieBase', baseName)
@@ -49,7 +55,15 @@ configuratorRouter
     .get("/delete-addon/:addonName", (req, res) => {
         const { addonName } = req.params;
 
-        const addons = getAddonsFromReq(req)
+        const oldAddons = getAddonsFromReq(req);
+
+        if(!oldAddons.includes(addonName)) {
+            return res.render('error', {
+                description: `Can not deleted something that is not already added to the cookie: ${addonName}`
+            })
+        }
+
+        const addons = oldAddons
             .filter(addon => addon !== addonName);
 
         res
